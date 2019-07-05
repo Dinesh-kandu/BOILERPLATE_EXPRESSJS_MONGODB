377649c8f224eb11a8969bee57fa744c562e6bf1
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-07-05 10:36:32 +0700

    prepare for deploy

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -3,38 +3,31 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const ProfileItem = ({
-  profile: {
+ profile: {
  user, status, company, location, skills,
 },
 }) => {
   return (
     <div className="profile bg-light">
-      <img
-        className="round-img"
-        src={user.avatar}
-        alt="avatar"
-      />
+      <img className="round-img" src={user.avatar} alt="avatar" />
       <div>
         <h2>{user.name}</h2>
         <p>
           {status}
           {' '}
           {company && (
-          <span>
-            {' '}
-at
-            {company}
-          </span>
-)}
+            <span>
+              {' '}
+              at
+              {company}
+            </span>
+          )}
         </p>
         <p className="my-1">
           {location && <span>{location}</span>}
           {' '}
         </p>
-        <Link
-          to={`/profile/${user._id}`}
-          className="btn btn-primary"
-        >
+        <Link to={`/profile/${user._id}`} className="btn btn-primary">
           View
         </Link>
       </div>
